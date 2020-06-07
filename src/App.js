808ffad9aab7a61127db07d0c8639078eec57868
Giribabu Nagipogu <giribabu.nagipogu@solutionec.com>
2020-06-07 14:01:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useReducer} from 'react';
 import './App.css';
+import ClassCounter from './components/ClassCounter';
+import HookCounter from './components/HookCounter';
+import HookCounterTwo from './components/HookCounterTwo';
+import HookCounterThree from './components/HookCounterThree';
+import HookCounterFour from './components/HookCounterFour';
+import ClassCounterOne from './components/ClassCounterOne';
+import HookCounterOne from './components/HookCounterOne';
+import ClassMouse from './components/ClassMouse';
+import HookMouse from './components/HookMouse';
+import MouseContainer from './components/MouseContainer';
+import IntervalClassCounter from './components/IntervalClassCounter';
+import IntervalHookCounter from './components/IntervalHookCounter';
+import DataFetching from './components/DataFetching';
+import CounterOne from './components/CounterOne';
+import CounterTwo from './components/CounterTwo';
+import CounterThree from './components/CounterThree';
+import ComponentA from './components/ComponentA'
+import ComponentB from './components/ComponentB';
+import ComponentC from './components/ComponentC';
+import DataFetchingOne from './components/DataFetchingOne';
+import DataFetchingTwo from './components/DataFetchingTwo';
+
+
+export const CountContext = React.createContext()
+
+const initialState = 0
+const reducer = (state, action) => {
+    switch (action) {
+        case 'increment':
+            return state + 1
+        case 'decrement':
+            return state - 1
+        case 'reset':
+            return initialState
+        default:
+            return state
+    }
+}
 
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState)
   return (
+    // <CountContext.Provider value={{countState: count, countDispatch: dispatch}}>
+
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DataFetchingTwo />
+      {/* <DataFetchingOne /> */}
+      {/* Count - {count} */}
+      {/* <ComponentC /> */}
+      {/* <ComponentB /> */}
+      {/* <ComponentA /> */}
+      {/* <CounterThree /> */}
+      {/* <CounterTwo /> */}
+      {/* <CounterOne /> */}
+      {/* <DataFetching /> */}
+      {/* <IntervalClassCounter /> */}
+      {/* <IntervalHookCounter /> */}
+      {/* <MouseContainer /> */}
+      {/* <HookMouse /> */}
+      {/* <ClassMouse /> */}
+      {/* <HookCounterOne /> */}
+      {/* <ClassCounterOne /> */}
+      {/* <HookCounterFour /> */}
+      {/* <HookCounterThree /> */}
+      {/* <HookCounterTwo /> */}
+      {/* <HookCounter /> */}
+       {/* <ClassCounter /> */}
     </div>
+    // </CountContext.Provider>
   );
 }
 
